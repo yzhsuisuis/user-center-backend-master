d4aad08fb8b1486b3b6477496ddb3b5ab63cb332
yang-zihe1235 <972849883@qq.com>
2024-09-03 21:23:18 +0800

    添加跨域注解

--- user_center/src/main/java/com/yupi/usercenter/UserCenterApplication.java
+++ user_center/src/main/java/com/yupi/usercenter/UserCenterApplication.java
@@ -3,10 +3,12 @@ package com.yupi.usercenter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 @SpringBootApplication
 @MapperScan("com.yupi.usercenter.mapper")
+@CrossOrigin(origins = {"http://106.54.235.189:8080"} ,allowCredentials = "true" )
 public class UserCenterApplication {
     public static void main(String[] args) {
         SpringApplication.run(UserCenterApplication.class,args);
